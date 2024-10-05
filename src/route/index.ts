a947a471f17d6841c93eaaae4bcdad831b9103bc
Shakir <shakirrahman81@gmail.com>
2024-10-04 22:19:04 -0400

    Removes game routes

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,14 +1,10 @@
 // src/route/index.ts
 import { Router } from 'express';
 import userRoutes from './userRoutes';
-import gameRoutes from './gameRoutes';
 
 const router = Router();
 
 // User-related routes
 router.use('/users', userRoutes);
-
-// Game-related routes
-router.use('/games', gameRoutes);
 
 export default router;
